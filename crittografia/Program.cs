53efd5d1b8e2c73a608c1e984b41a5508166298b
contidavide-sudo <conti.davide@franchettisalviani.net>
2025-12-03 13:36:13 +0100

    Aggiungere i file di progetto.

---
+++ crittografia/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace crittografia
+{
+    internal class Program
+    {
+        static char[] Sostituzione(char[] alfabeto, char[] nome, int k)
+        {
+
+            for(int i = 0; i < nome.Length; i++)
+            {
+                int indAlf = 0;
+
+                while (nome[i] != alfabeto[indAlf])
+                {                                      
+                   indAlf++;                   
+                }
+
+                nome[i] = alfabeto[indAlf + k];
+
+            }
+            return nome;
+        }
+        static void Main(string[] args)
+        {
+            int key = 4;
+            char[] alfa = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+            string parola;
+
+            Console.Write("Inserisci la parola: ");
+            parola = Console.ReadLine();
+
+            char[] parolaArray = parola.ToCharArray();
+
+            char[] parolaSostituzione = Sostituzione(alfa, parolaArray, key);
+
+            for (int i = 0; i < parolaSostituzione.Length; i++)
+            {
+                Console.Write(parolaSostituzione[i]);  
+            }      
+        }
+    }
+}
